61bba6e839f755227c95edf010e6f6a6dd932483
99MusashiMiyamoto <siw23010020@class.siw.ac.jp>
2025-08-29 11:32:21 +0900

    fix: resolve build error and warnings

--- src/pages/profile/[userId].tsx
+++ src/pages/profile/[userId].tsx
@@ -1,16 +1,27 @@
 import apiClient from '@/lib/apiClients';
 import Post from '@/components/Post';
 import { PostType, UserType } from '@/types';
-import { GetServerSideProps } from 'next';
+// import { GetServerSideProps } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Image from "next/image";
 import React from 'react'
+import { ParsedUrlQuery } from 'querystring';
 
 type Props = {
     user: UserType;
 };
 
-export const getServerSideProps: GetServerSideProps = async (context: any) => {
-    const { userId } = context.query;
-    
+// export const getServerSideProps: GetServerSideProps = async (context: any) => {
+interface Params extends ParsedUrlQuery {
+  userId: string;
+}
+
+export const getServerSideProps: GetServerSideProps<Props, Params> = async (context) => {
+
+  
+    // const { userId } = context.query;
+     const { userId } = context.params!;
+
     try {
       // この単一のエンドポイントでユーザープロフィールと投稿が返されるはずです
       const response = await apiClient.get(`/users/profile/${userId}`);
@@ -34,10 +45,12 @@ const UserProfile = ({ user }: Props) => {
       <div className="w-full max-w-xl mx-auto">
         <div className="bg-white shadow-md rounded-lg p-6 mb-4">
           <div className="flex items-center">
-            <img
+            <Image
               className="w-20 h-20 rounded-full mr-4"
               alt="User Avatar"
               src={user.profile?.profileImageUrl || '/default-profile.png'}
+              width={80}
+              height={80}
             />
             <div>
               <h2 className="text-2xl font-semibold mb-1">{user.username}</h2>
